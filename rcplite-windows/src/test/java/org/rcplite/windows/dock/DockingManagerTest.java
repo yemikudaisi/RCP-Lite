ec9699326ad6413d8c3d44d7b11a87f3cca76908
Yemi Kudaisi <yemikudaisi@gmail.com>
2023-08-27 00:04:15 +0100

    Added annotation utilities

--- rcplite-windows/src/test/java/org/rcplite/windows/dock/DockingManagerTest.java
+++ rcplite-windows/src/test/java/org/rcplite/windows/dock/DockingManagerTest.java
@@ -1,9 +1,28 @@
 package org.rcplite.windows.dock;
 
 import junit.framework.TestCase;
+import org.rcplite.windows.ViewComponent;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class DockingManagerTest extends TestCase {
 
     public void testCalculateGridSize() {
+        Method method = null;
+        try {
+            method = ViewComponent.Configuration.class.getDeclaredMethod("position", Integer[].class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        method.setAccessible(true);
+
+        Field annotations = annotationData.getClass().getDeclaredField(ANNOTATIONS);
+        annotations.setAccessible(true);
+
+        Map<Class<? extends Annotation>, Annotation> map = annotations.get(annotationData);
+        map.put(targetAnnotation, targetValue);
+
+        assertTrue(true);
     }
 }
