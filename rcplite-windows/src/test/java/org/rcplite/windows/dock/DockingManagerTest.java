ceb11f075d3ddc9cddf5712fee2f673f64eb302f
Yemi Kudaisi <yemikudaisi@gmail.com>
2023-08-28 23:57:22 +0100

    typo

--- rcplite-windows/src/test/java/org/rcplite/windows/dock/DockingManagerTest.java
+++ rcplite-windows/src/test/java/org/rcplite/windows/dock/DockingManagerTest.java
@@ -9,19 +9,19 @@ import java.lang.reflect.Method;
 public class DockingManagerTest extends TestCase {
 
     public void testCalculateGridSize() {
-        Method method = null;
-        try {
-            method = ViewComponent.Configuration.class.getDeclaredMethod("position", Integer[].class);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-        method.setAccessible(true);
-
-        Field annotations = annotationData.getClass().getDeclaredField(ANNOTATIONS);
-        annotations.setAccessible(true);
-
-        Map<Class<? extends Annotation>, Annotation> map = annotations.get(annotationData);
-        map.put(targetAnnotation, targetValue);
+//        Method method = null;
+//        try {
+//            method = ViewComponent.Configuration.class.getDeclaredMethod("position", Integer[].class);
+//        } catch (NoSuchMethodException e) {
+//            throw new RuntimeException(e);
+//        }
+//        method.setAccessible(true);
+//
+//        Field annotations = annotationData.getClass().getDeclaredField(ANNOTATIONS);
+//        annotations.setAccessible(true);
+//
+//        Map<Class<? extends Annotation>, Annotation> map = annotations.get(annotationData);
+//        map.put(targetAnnotation, targetValue);
 
         assertTrue(true);
     }
